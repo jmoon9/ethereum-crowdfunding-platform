7a0189fefbd52cc3583332b0d12ebc222a462528
jmoon9 <47996673+jmoon9@users.noreply.github.com>
2020-06-25 11:36:16 -0500

    Start script tweak

--- server.js
+++ server.js
@@ -11,10 +11,5 @@ const port = process.env.PORT || 3000;
 app.prepare().then(() => {
     createServer(handler).listen(port, (err) => {
         if(err) throw err;
-        console.log('Listening on ', port);
     });
 });
-
-
-//0x62dB26E61712Fc4b6da332D7E77dff9EF1c96A84
-//0x62dB26E61712Fc4b6da332D7E77dff9EF1c96A84
